9a2a4739b9297d100cacd9e957374bef858584dd
Masata-code <ctm@masata-nkmr.jp>
2022-01-20 19:54:59 +0900

    作品詳細Swiperテスト

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,27 +1,14 @@
-var slider = new Swiper ('.swiper-container', {
-      slidesPerView: 1,
-      centeredSlides: true,
-      loop: true,
-      loopedSlides: 9, //スライドの枚数と同じ値を指定
-      autoplay: {
-        delay: 2000,
-      },
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-      },
-    });
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+});
 
-    //サムネイル
-    var thumbs = new Swiper ('.gallery-thumbs', {
-        slidesPerView: 'auto',
-        spaceBetween: 10,
-        centeredSlides: true,
-        loop: true,
-        slideToClickedSlide: true,
-    });
-
-    //4系～
-    //メインとサムネイルを紐づける
-    slider.controller.control = thumbs;
-    thumbs.controller.control = slider;
+const swiper2 = new Swiper(".mySwiper2", {
+  thumbs: {
+    swiper: swiper,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
