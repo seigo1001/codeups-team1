af49cb3adab27e439a548f14403bac0d4424fcbd
Masata-code <ctm@masata-nkmr.jp>
2022-01-26 21:41:41 +0900

    テスト

--- src/js/script.js
+++ src/js/script.js
@@ -78,3 +78,35 @@ jQuery(function ($) {
     return false;
   });
 });
+
+var searchItem = ".search__item"; //絞り込み項目を選択するエリア
+var listItem = ".p-blog__item"; //絞り込み対象のアイテム
+var hideClass = "is-hide"; //絞り込み対象外の場合に付与されるclass名
+var activeClass = "is-active"; //選択中のグループに付与されるclass名
+
+$(function () {
+  //絞り込みを変更した時
+  $(searchItem).on("click", function () {
+    $(searchItem).removeClass(activeClass);
+    var group = $(this).addClass(activeClass).data("group");
+    search_filter(group);
+  });
+});
+
+function search_filter(group) {
+  //非表示状態を解除
+  $(listItem).removeClass(hideClass);
+  //値が空の場合はすべて表示
+  if (group === "") {
+    return;
+  }
+  //リスト内の各アイテムをチェック
+  for (var i = 0; i < $(listItem).length; i++) {
+    //アイテムに設定している項目を取得
+    var itemData = $(listItem).eq(i).data("group");
+    //絞り込み対象がどうかを調べる
+    if (itemData !== group) {
+      $(listItem).eq(i).addClass(hideClass);
+    }
+  }
+}
